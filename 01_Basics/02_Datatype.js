141136774057227d25a07baa9e2f7e65899d09a8
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-19 17:13:12 +0000

    DataTypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // Treat all JS Code as new Version
+
+let name = "Kuldeep"
+let age = 27
+let isWorking = true
+let state;
+
+//number 2 to power 53
+//BigInt
+//Bool
+//String
+//null => Standalone value or empty
+//undefiend => not yet assigned
+//Symbol => Used to find elemet by uniquely
+
+//Object
+console.log(typeof null);
+console.log(typeof undefined);
