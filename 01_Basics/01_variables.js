6fdb93880411ca06b72fd05865c123923e92976b
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-19 16:43:20 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "basican.kuldeep@gmail.com"
+var accountPassword = "dasdas"
+accountCity = "Panchkula"
+let accountState
+
+/* 
+const can never be changed once assigned
+
+let can be changable
+
+var used in old days of javascript but it has a problem in block scope and functional scope
+
+accountId = 123  <----- Not allowed
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
