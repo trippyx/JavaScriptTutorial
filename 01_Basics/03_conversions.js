2732833edda04724472fe25db159eea92a598596
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-19 18:31:52 +0000

    Converionsfinal

--- 01_Basics/03_conversions.js
+++ 01_Basics/03_conversions.js
@@ -22,4 +22,44 @@ String to Bool
 
 */
 
+// ************ Operations **********
 
+let value = 3
+let negValue = -value
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.1og(2%3)
+
+let str1 = "Kuldeep"
+let str2 = " Singh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(3 + "4");
+console.log("3" + 4);
+console.log("3" + "4");
+console.log(3 + "4" + "4");
+console.log(3 + 2 +"4");
+console.log(+true);
+console.log(+"");
+
+//Comparison
+
+//Equlity check and comparision work differently in js so >= convet null to 0 and other will not
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+//Strict type checking
+
+console.log(undefined === 0);
