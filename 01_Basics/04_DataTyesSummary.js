6c0509544274794841900d7bb10e6cea42edcd33
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-19 18:46:04 +0000

    SummaryDataTypes

---
+++ 01_Basics/04_DataTyesSummary.js
@@ -0,0 +1,20 @@
+ /*
+ Primitive
+ String , Number ,Boolean , Null , Undefiend , Symbol , BigInt
+
+Non Primitive (Refrence)
+Array
+Objects
+Functions
+
+
+ */
+
+const ars = ["sdad","dsdsa"]
+
+let myFunc = function(){
+    console.log("sdsad")
+}
+
+console.log(typeof ars);
+console.log(typeof myFunc);
