a814e3dddddabd1d94c854481af17d2202e54229
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-20 19:09:49 +0000

    Scopess

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -12,3 +12,16 @@ if(true){
 console.log(a);
 console.log(b);
 console.log(c);
+
+
+console.log(names(10))
+function names(number){
+    return number
+}
+
+
+const valse = function(nuber){
+    return nuber
+}
+
+console.log(valse(10))
