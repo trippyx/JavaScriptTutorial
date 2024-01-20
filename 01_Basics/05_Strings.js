c423521dc787305427fea10d42d02edb95c17d63
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-20 12:56:54 +0000

    Stringsss

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,18 @@
+const names = "Kuldeep"
+
+const repoCount = 50
+
+// console.log(names + repoCount);
+
+console.log(`Hello my name is ${names} and i have repo count of ${repoCount}}`) //String interpolation
+
+
+const gameName = new String("Kuldeep-Singh")
+
+console.log(gameName);
+
+console.log(gameName.charAt(2))
+console.log(gameName.toUpperCase())
+console.log(gameName.indexOf('K'));
+console.log(gameName.split('-',1))
+console.log(gameName.slice(0,5));
