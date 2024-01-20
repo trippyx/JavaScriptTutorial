4e7400c0037943eb2d48c21262848ce9eb86c34b
Kuldeep Singh <33547942+trippyx@users.noreply.github.com>
2024-01-20 13:14:40 +0000

    NumsMaths

---
+++ 01_Basics/06_Nums_Maths.js
@@ -0,0 +1,22 @@
+const number = 100
+
+const bankBalance = new Number(number)
+
+console.log(bankBalance);
+console.log(bankBalance.toString());
+console.log(bankBalance.toFixed(1)); //Used to display how much presion values
+
+const newValue = 20.55
+
+console.log(newValue.toPrecision(2));
+
+const hundreds = 10000000
+
+
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+//*********************************Maths **********/
+
+Math.random()
